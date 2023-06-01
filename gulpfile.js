4a11597c3c989bdf3588305655987ed1c1210ae4
carlospaprica <carlos@papricacomunicacao.com.br>
2023-06-01 14:21:55 -0300

    r

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 
-gulp.task('minify-css', function () {
-  return gulp.src('src/css/*.css')
+gulp.task('compile-scss', function () {
+  return gulp.src('src/css/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(cssmin())
-    .pipe(rename({ suffix: '.min' })) // Adiciona o sufixo .min ao arquivo CSS minificado
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('src/css/*.css', gulp.series('minify-css')); // Observa os arquivos CSS na pasta src/css
+  gulp.watch('src/css/*.scss', gulp.series('compile-scss'));
 });
 
-gulp.task('default', gulp.series('minify-css', 'watch'));
+gulp.task('default', gulp.series('compile-scss', 'watch'));
