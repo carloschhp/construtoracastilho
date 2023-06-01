e5125e35774e819c78265d717ee50392acd5b718
Carlos <carloshenriquechhp@gmail.com>
2023-06-01 01:00:25 -0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 const gulp = require('gulp');
 const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 gulp.task('minify-css', function () {
   return gulp.src('src/css/*.css')
     .pipe(cssmin())
+    .pipe(rename({ suffix: '.min' })) // Adiciona o sufixo .min ao arquivo CSS minificado
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('default', gulp.series('minify-css'));
+gulp.task('watch', function () {
+  gulp.watch('src/css/*.css', gulp.series('minify-css')); // Observa os arquivos CSS na pasta src/css
+});
+
+gulp.task('default', gulp.series('minify-css', 'watch'));
