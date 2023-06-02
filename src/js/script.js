eb915e466a9f76709007534135a56048699608e8
carlospaprica <carlos@papricacomunicacao.com.br>
2023-06-02 02:04:14 -0300

    02:04

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,47 @@
 $(document).ready(function () {
     $(".dropdown-item").on("click", function (e) {
-        e.preventDefault();
-        var target = $(this).data("target");
-        $(target).fadeIn();
+      e.preventDefault();
+      var target = $(this).data("target");
+      $(target).fadeIn();
     });
 
     $('.slider').slick({
-        dots: false,
-        arrows: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: '<button type="button" class="slick-prev">Anterior</button>',
-        nextArrow: '<button type="button" class="slick-next">Próximo</button>'
+      dots: false,
+      arrows: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: '<button type="button" class="slick-prev">Anterior</button>',
+      nextArrow: '<button type="button" class="slick-next">Próximo</button>'
     });
-});
 
-document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = $('.sidebar');
+    const sidebarToggle = $('.sidebar-toggle');
+
+    sidebarToggle.click(function () {
+      sidebar.toggle('slide', { direction: 'left' }, 500);
+    });
+
     var contentItems = document.querySelectorAll('.content-item');
     for (var i = 1; i < contentItems.length; i++) {
-        contentItems[i].style.display = 'none';
+      contentItems[i].style.display = 'none';
     }
+
     function showContent(contentId) {
-        // Oculta todos os content-items
-        var contentItems = document.querySelectorAll('.content-item');
-        contentItems.forEach(function (item) {
-            item.style.display = 'none';
-        });
+      // Oculta todos os content-items
+      var contentItems = document.querySelectorAll('.content-item');
+      contentItems.forEach(function (item) {
+        item.style.display = 'none';
+      });
 
-        // Exibe o content-item correspondente ao slider-item clicado
-        var selectedContent = document.getElementById(contentId);
-        selectedContent.style.display = 'block';
+      // Exibe o content-item correspondente ao slider-item clicado
+      var selectedContent = document.getElementById(contentId);
+      selectedContent.style.display = 'block';
 
-        // Rola suavemente para o conteúdo exibido
-        var offsetTop = selectedContent.offsetTop;
-        window.scrollTo({
-            top: offsetTop,
-            behavior: "smooth"
-        });
+      // Rola suavemente para o conteúdo exibido
+      var offsetTop = selectedContent.offsetTop;
+      window.scrollTo({
+        top: offsetTop,
+        behavior: "smooth"
+      });
     }
-});
+  });
